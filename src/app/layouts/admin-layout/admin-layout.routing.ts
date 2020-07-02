00344741399e799293dbbf3be73fd0ed6bb2b60e
Nick Carlos Mesias Rivera <nickmeza@upeu.edu.pe>
2020-07-01 23:43:54 -0500

    detalle conv

--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -8,6 +8,7 @@ import { UserComponent } from "../../pages/user/user.component";
 import { TablesComponent } from "../../pages/tables/tables.component";
 import { TypographyComponent } from "../../pages/typography/typography.component";
 import { ConvocatoriaCRUDComponent } from "../../pages/convocatoria-crud/convocatoria-crud.component";
+import { DetalleconvocatoriaComponent } from "../../pages/detalleconvocatoria/detalleconvocatoria.component";
 // import { RtlComponent } from "../../pages/rtl/rtl.component";
 //import { InicioComponent } from "../../md-inicio/inicio/inicio.component";
 //import { ConveniosComponent } from "../../md-inicio/convenios/convenios.component";
@@ -28,6 +29,8 @@ export const AdminLayoutRoutes: Routes = [
   { path: "typography", component: TypographyComponent ,
   canActivate:[AuthGuard]},
   { path: "convocatoriamin", component: ConvocatoriaCRUDComponent ,
+  canActivate:[AuthGuard]},
+  { path: "detalleconv", component: DetalleconvocatoriaComponent ,
   canActivate:[AuthGuard]},
   //{ path: "inicio", component: InicioComponent },
   //{ path: "convenios", component: ConveniosComponent },
---
+++ src/app/pages/detalleconvocatoria/detalleconvocatoria.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detalleconvocatoria',
+  templateUrl: './detalleconvocatoria.component.html',
+  styleUrls: ['./detalleconvocatoria.component.scss']
+})
+export class DetalleconvocatoriaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
