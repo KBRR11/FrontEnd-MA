1c333b231cd9f43db541dfcf05ac64819a2460d1
johan <johanmamani@upeu.edu.pe>
2020-07-05 12:29:12 -0500

    no se que ah pasado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,14 +15,10 @@ import { AppRoutingModule } from "./app-routing.module";
 import { ComponentsModule } from "./components/components.module";
 import { CabezaComponent } from './md-inicio/cabeza/cabeza.component';
 import { LoginComponent } from './login/login.component';
-<<<<<<< HEAD
 import { RegisterPerComponent } from './Registro-Usuarios/register-per/register-per.component';
 import { RegisterUSERComponent } from './Registro-Usuarios/register-user/register-user.component';
 import { DatosAcademicosComponent } from './Registro-Usuarios/datos-academicos/datos-academicos.component';
 
-=======
-import { OpcionComponent } from './pages/opcion/opcion/opcion.component';
->>>>>>> 0d85d8c8526f090ac41ffbf05d3b51563228f522
 
 
 
@@ -37,11 +33,7 @@ import { OpcionComponent } from './pages/opcion/opcion/opcion.component';
     AppRoutingModule,
     ToastrModule.forRoot()
   ],
-<<<<<<< HEAD
   declarations: [AppComponent, AdminLayoutComponent, AuthLayoutComponent, CabezaComponent, LoginComponent, RegisterPerComponent, RegisterUSERComponent, DatosAcademicosComponent, ],
-=======
-  declarations: [AppComponent, AdminLayoutComponent, AuthLayoutComponent, CabezaComponent, LoginComponent, OpcionComponent, ],
->>>>>>> 0d85d8c8526f090ac41ffbf05d3b51563228f522
   providers: [],
   bootstrap: [AppComponent]
 })
