018d11b8f3f246823393ad08ef76e525b7263d16
unknown <richeti1998@gmail.com>
2020-07-09 20:58:14 -0500

    user

--- src/app/Modelo/Usuarios.ts
+++ src/app/Modelo/Usuarios.ts
@@ -18,3 +18,16 @@ export class Usuarios{
     color_fondo:string;
     color_menu:string;
 }
+
+export class Usuario{
+    idusuario:number;
+    codigo:string;
+    usuario:string;
+    password:string;
+    sede:number;
+    tipo:number;
+    estado:number;
+    idpersona: number;
+    idep:number;
+    ciclo:string;
+}
